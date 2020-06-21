76f2899aeaa42f49d72df1737db24fc632e4c88b
Hyuk Myeong <hyuk.myeong@lge.com>
2020-06-21 23:56:22 +0900

    finish TRIANGLEPATH

---
+++ ch08/code_8_TRIAGNLEPATH_2.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+#include <sstream>
+
+using namespace std;
+
+#if 0
+#if 0
+const int N = 5;
+int board[N][N] = {
+  { 6, 0, 0, 0, 0 },
+  { 1, 2, 0, 0, 0 },
+  { 3, 7, 4, 0, 0 },
+  { 9, 4, 1, 7, 0 },
+  { 2, 7, 5, 9, 4 },
+};
+#else
+const int N = 5;
+int board[N][N] = {
+  {   1,   0,   0,   0,   0 },
+  {   2,   4,   0,   0,   0 },
+  {   8,  16,   8,   0,   0 },
+  {  32,  64,  32,  64,   0 },
+  { 128, 256, 128, 256, 128 },
+};
+#endif
+#endif
+
+vector<vector<vector<int>>> g_data;
+vector<vector<int>> g_board;
+
+void get_data()
+{
+  int cases = 0;
+  cin >> cases;
+
+  for (int i = 0; i < cases; ++i) {
+    int lines = 0;
+    cin >> lines;
+    cin.ignore();
+
+    vector<vector<int>> v1;
+    for (int j = 0; j < lines; ++j) {
+      string line, num;
+      getline(cin, line);
+      istringstream ss(line);
+
+      vector<int> v2(lines, 0);
+      auto&& iter = v2.begin();
+      while (getline(ss, num, ' ')) {
+        *iter = atoi(num.c_str());
+        ++iter;
+      }
+      v1.push_back(v2);
+    }
+    g_data.push_back(v1);
+  }
+}
+
+int cache[100][100];
+
+int triangle(unsigned int x, unsigned int y)
+{
+  int& res = cache[y][x];
+
+  if (res != -1)
+    return cache[y][x];
+
+  if (x == g_board.size() - 1 || y == g_board.size() - 1)
+    return g_board[y][x];
+
+  res = max(triangle(x, y + 1), triangle(x + 1, y + 1)) + g_board[y][x];
+
+  return res;
+}
+
+int main()
+{
+  get_data();
+
+  for (auto& board : g_data) {
+    memset(cache, -1, sizeof(cache));
+    g_board.clear();
+    g_board = board;
+    cout << triangle(0, 0) << endl;
+  }
+}
--- ch08/main.cpp
+++ ch08/main.cpp
@@ -1,84 +1,13 @@
 #include <iostream>
 #include <vector>
-#include <memory>
+#include <cmath>
 #include <cstring>
-#include <algorithm>
+#include <cstdlib>
+#include <sstream>
 
 using namespace std;
 
-class problem {
-public:
-  string pattern;
-  vector<string> words;
-};
-vector<problem> g_problems;
-string g_pattern;
-string g_word;
-
-void get_input()
-{
-  int cnt;
-  cin >> cnt;
-
-  for (int i = 0; i < cnt; i++) {
-    problem p;
-    int cnt_words;
-    cin >> p.pattern;
-    cin >> cnt_words;
-
-    for (int j = 0; j < cnt_words; j++) {
-      string input;
-      cin >> input;
-      p.words.push_back(input);
-    }
-    sort(p.words.begin(), p.words.end());
-    g_problems.push_back(p);
-  }
-}
-
-int cache[101][101];
-
-bool match(unsigned int pattern_idx, unsigned int word_idx)
-{
-  int& res = cache[pattern_idx][word_idx];
-
-  if (res != -1)
-    return res;
-
-  while ((pattern_idx < g_pattern.size() && word_idx < g_word.size()) &&
-        (g_pattern[pattern_idx] == g_word[word_idx] || g_pattern[pattern_idx] == '?'))
-    match(++pattern_idx, ++word_idx);
-
-  if (pattern_idx == g_pattern.size() && word_idx == g_word.size()) {
-    res = true;
-    return res;
-  }
-
-  if (g_pattern[pattern_idx] == '*') {
-    if (match(pattern_idx + 1, word_idx) || match(pattern_idx, word_idx + 1)) {
-      res = true;
-      return res;
-    }
-  }
-
-  res = false;
-  return res;
-}
-
 int main()
 {
-  get_input();
-
-  for (auto problem : g_problems) {
-    g_pattern = problem.pattern;
-
-    for (auto& word : problem.words) {
-      memset(cache, -1, sizeof(cache));
-      g_word = word;
-
-      if (match(0, 0))
-        cout << g_word << endl;
-    }
-  }
-  return 0;
+  cout << "LIS" << endl;
 }
