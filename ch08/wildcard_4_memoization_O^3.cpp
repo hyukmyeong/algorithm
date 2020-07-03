af6480de70e67e6951974664742b4402442f3303
Hyuk Myeong <hyuk.myeong@lge.com>
2020-07-03 13:17:13 +0900

    Update wildcard_4_memoization_O^3.cpp

--- ch08/wildcard_4_memoization_O^3.cpp
+++ ch08/wildcard_4_memoization_O^3.cpp
@@ -2,20 +2,21 @@
 #include <vector>
 #include <memory>
 #include <cstring>
+#include <algorithm>
 
 using namespace std;
 
 class problem {
-public:
-  string pattern;
-  vector<string> words;
+    public:
+        string pattern;
+        vector<string> words;
 };
 int cnt;
 vector<problem> problems;
 
 problem g_problem;
 string g_pattern;
-string g_word;
+string g_input;
 
 int cache[101][101];
 
@@ -58,54 +59,42 @@ bool match(unsigned int pattern_idx, unsigned int input_idx)
 
 void get_input()
 {
-  cin >> cnt;
+    cin >> cnt;
 
-  for (int i = 0; i < cnt; i++) {
-    problem p;
-    int cnt_inputs;
-    cin >> p.pattern;
-    cin >> cnt_inputs;
+    for (int i = 0; i < cnt; i++) {
+        problem p;
+        int cnt_inputs;
+        cin >> p.pattern;
+        cin >> cnt_inputs;
 
-    for (int j = 0; j < cnt_inputs; j++) {
-      string input;
-      cin >> input;
-      p.words.push_back(input);
+        for (int j = 0; j < cnt_inputs; j++) {
+            string input;
+            cin >> input;
+            p.words.push_back(input);
+        }
+        problems.push_back(p);
     }
-    problems.push_back(p);
-  }
 }
 
 int main()
 {
-#if 0
-#if 1
-  g_problem.pattern = "***";
-  g_problem.words.push_back("abcde");
-#else
-  g_problem.pattern = "*p*";
-  g_problem.words.push_back("help");
-  g_problem.words.push_back("papa");
-  g_problem.words.push_back("hello");
-#endif
-#else
-#if 0
-  g_problem.pattern = "*bb*";
-  g_problem.words.push_back("babbbc");
-#else
-  g_problem.pattern = "he?p";
-  g_problem.words.push_back("help");
-  g_problem.words.push_back("heap");
-  g_problem.words.push_back("helpp");
-#endif
-#endif
+    get_input();
 
-  g_pattern = g_problem.pattern;
+    for (auto& problem : problems) {
+        g_pattern = problem.pattern;
 
-  for (auto& item : g_problem.words) {
-    memset(cache, -1, sizeof(cache));
-    g_word = item;
-    if (match(0, 0))
-      cout << g_word << endl;
-  }
-  return 0;
+        vector<string> result;
+        for (auto& item : problem.words) {
+            memset(cache, -1, sizeof(cache));
+            g_input = item;
+            if (match(0, 0))
+                result.push_back(item);
+        }
+        sort(result.begin(), result.end());
+        for (auto& res : result)
+            cout << res << endl;
+
+    }
+
+    return 0;
 }
