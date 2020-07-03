459dc1f8708747daa085742b4db807ccc79c1e09
Hyuk Myeong <hyuk.myeong@lge.com>
2020-07-03 22:54:05 +0900

    wow wow

--- ch08/triangle_1_bad_memoization.cpp
+++ ch08/triangle_1_bad_memoization.cpp
@@ -10,11 +10,11 @@ int const max_value  = 9;
 int cache[size][size][max_value * size];
 
 int board[size][size] = {
-  { 6, 0, 0, 0, 0 },
-  { 1, 2, 0, 0, 0 },
-  { 3, 7, 4, 0, 0 },
-  { 9, 4, 1, 7, 0 },
-  { 2, 7, 5, 9, 4 },
+    { 6, 0, 0, 0, 0 },
+    { 1, 2, 0, 0, 0 },
+    { 3, 7, 4, 0, 0 },
+    { 9, 4, 1, 7, 0 },
+    { 2, 7, 5, 9, 4 },
 };
 
 int triangle(int x, int y, int sum)
@@ -22,13 +22,13 @@ int triangle(int x, int y, int sum)
     if (y == size-1)
         return sum + board[y][x];
 
-    int& ret = cache[y][x][sum];
-    if (ret != -1)
-        return ret;
-    
+    int& res = cache[y][x][sum];
+    if (res != -1)
+        return res;
+
     sum += board[y][x];
-    ret = max(triangle(x, y+1, sum), triangle(x+1, y+1, sum));
-    return ret;
+    res = max(triangle(x, y+1, sum), triangle(x+1, y+1, sum));
+    return res;
 }
 
 int main()
@@ -43,40 +43,40 @@ vector<vector<int>> g_board;
 
 void get_data()
 {
-  int cases = 0;
-  cin >> cases;
+    int cases = 0;
+    cin >> cases;
 
-  for (int i = 0; i < cases; ++i) {
-    int lines = 0;
-    cin >> lines;
-    cin.ignore();
+    for (int i = 0; i < cases; ++i) {
+        int lines = 0;
+        cin >> lines;
+        cin.ignore();
 
-    vector<vector<int>> v1;
-    for (int j = 0; j < lines; ++j) {
-      string line, num;
-      getline(cin, line);
-      istringstream ss(line);
+        vector<vector<int>> v1;
+        for (int j = 0; j < lines; ++j) {
+            string line, num;
+            getline(cin, line);
+            istringstream ss(line);
 
-      vector<int> v2(lines, 0);
-      auto&& iter = v2.begin();
-      while (getline(ss, num, ' ')) {
-        *iter = atoi(num.c_str());
-        ++iter;
-      }
-      v1.push_back(v2);
+            vector<int> v2(lines, 0);
+            auto&& iter = v2.begin();
+            while (getline(ss, num, ' ')) {
+                *iter = atoi(num.c_str());
+                ++iter;
+            }
+            v1.push_back(v2);
+        }
+        g_data.push_back(v1);
     }
-    g_data.push_back(v1);
-  }
 }
 
 int main()
 {
-  get_data();
+    get_data();
 
-  for (auto& board : g_data) {
-    g_board.clear();
-    g_board = board;
-    cout << triangle(0, 0) << endl;
-  }
+    for (auto& board : g_data) {
+        g_board.clear();
+        g_board = board;
+        cout << triangle(0, 0) << endl;
+    }
 }
 #endif
--- main.cpp
+++ main.cpp
@@ -7,34 +7,33 @@ using namespace std;
 
 int const size = 5;
 int const max_value  = 9;
-int cache[size][size][max_value * size];
+int cache[size][size];
 
 int board[size][size] = {
-  { 6, 0, 0, 0, 0 },
-  { 1, 2, 0, 0, 0 },
-  { 3, 7, 4, 0, 0 },
-  { 9, 4, 1, 7, 0 },
-  { 2, 7, 5, 9, 4 },
+    { 6, 0, 0, 0, 0 },
+    { 1, 2, 0, 0, 0 },
+    { 3, 7, 4, 0, 0 },
+    { 9, 4, 1, 7, 0 },
+    { 2, 7, 5, 9, 4 },
 };
 
-int triangle(int x, int y, int sum)
+int triangle(int x, int y)
 {
     if (y == size-1)
-        return sum + board[y][x];
+        return board[y][x];
 
-    int& ret = cache[y][x][sum];
-    if (ret != -1)
-        return ret;
-    
-    sum += board[y][x];
-    ret = max(triangle(x, y+1, sum), triangle(x+1, y+1, sum));
-    return ret;
+    int& res = cache[y][x];
+    if (res != -1)
+        return cache[y][x];
+
+    res = max(triangle(x, y+1), triangle(x+1, y+1)) + board[y][x];
+    return res;
 }
 
 int main()
 {
     memset(cache, -1, sizeof(cache));
-    cout << triangle(0, 0, 0) << endl;
+    cout << triangle(0, 0) << endl;
 }
 
 #if 0
@@ -43,40 +42,40 @@ vector<vector<int>> g_board;
 
 void get_data()
 {
-  int cases = 0;
-  cin >> cases;
+    int cases = 0;
+    cin >> cases;
 
-  for (int i = 0; i < cases; ++i) {
-    int lines = 0;
-    cin >> lines;
-    cin.ignore();
+    for (int i = 0; i < cases; ++i) {
+        int lines = 0;
+        cin >> lines;
+        cin.ignore();
 
-    vector<vector<int>> v1;
-    for (int j = 0; j < lines; ++j) {
-      string line, num;
-      getline(cin, line);
-      istringstream ss(line);
+        vector<vector<int>> v1;
+        for (int j = 0; j < lines; ++j) {
+            string line, num;
+            getline(cin, line);
+            istringstream ss(line);
 
-      vector<int> v2(lines, 0);
-      auto&& iter = v2.begin();
-      while (getline(ss, num, ' ')) {
-        *iter = atoi(num.c_str());
-        ++iter;
-      }
-      v1.push_back(v2);
+            vector<int> v2(lines, 0);
+            auto&& iter = v2.begin();
+            while (getline(ss, num, ' ')) {
+                *iter = atoi(num.c_str());
+                ++iter;
+            }
+            v1.push_back(v2);
+        }
+        g_data.push_back(v1);
     }
-    g_data.push_back(v1);
-  }
 }
 
 int main()
 {
-  get_data();
+    get_data();
 
-  for (auto& board : g_data) {
-    g_board.clear();
-    g_board = board;
-    cout << triangle(0, 0) << endl;
-  }
+    for (auto& board : g_data) {
+        g_board.clear();
+        g_board = board;
+        cout << triangle(0, 0) << endl;
+    }
 }
 #endif
