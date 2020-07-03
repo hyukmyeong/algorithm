c3c8844e5a9f873ad6a9fecf3111d7e4112fe872
Hyuk Myeong <hyuk.myeong@lge.com>
2020-07-03 15:08:18 +0900

    Update wildcard_6_memoization_O^2.cpp

--- ch08/wildcard_6_memoization_O^2.cpp
+++ ch08/wildcard_6_memoization_O^2.cpp
@@ -7,78 +7,87 @@
 using namespace std;
 
 class problem {
-public:
-  string pattern;
-  vector<string> words;
+    public:
+        string pattern;
+        vector<string> words;
 };
-vector<problem> g_problems;
+int cnt;
+vector<problem> problems;
+
+problem g_problem;
 string g_pattern;
-string g_word;
-
-void get_input()
-{
-  int cnt;
-  cin >> cnt;
-
-  for (int i = 0; i < cnt; i++) {
-    problem p;
-    int cnt_words;
-    cin >> p.pattern;
-    cin >> cnt_words;
-
-    for (int j = 0; j < cnt_words; j++) {
-      string input;
-      cin >> input;
-      p.words.push_back(input);
-    }
-    sort(p.words.begin(), p.words.end());
-    g_problems.push_back(p);
-  }
-}
+string g_input;
 
 int cache[101][101];
 
-bool match(unsigned int pattern_idx, unsigned int word_idx)
+bool match(unsigned int pattern_idx, unsigned int input_idx)
 {
-  int& res = cache[pattern_idx][word_idx];
+    int& res = cache[pattern_idx][input_idx];
 
-  if (res != -1)
-    return res;
+    if (res != -1)
+        return res;
 
-  while ((pattern_idx < g_pattern.size() && word_idx < g_word.size()) &&
-        (g_pattern[pattern_idx] == g_word[word_idx] || g_pattern[pattern_idx] == '?'))
-    match(++pattern_idx, ++word_idx);
-
-  if (pattern_idx == g_pattern.size() && word_idx == g_word.size()) {
-    res = true;
-    return res;
-  }
-
-  if (g_pattern[pattern_idx] == '*') {
-    if (match(pattern_idx + 1, word_idx) || match(pattern_idx, word_idx + 1)) {
-      res = true;
-      return res;
+    if (pattern_idx < g_pattern.size() && input_idx < g_input.size()) {
+        if (g_pattern[pattern_idx] == g_input[input_idx] || g_pattern[pattern_idx] == '?') {
+            res = match(pattern_idx + 1, input_idx + 1);
+            return res;
+        }
     }
-  }
 
-  res = false;
-  return res;
+    if (pattern_idx == g_pattern.size() && input_idx == g_input.size()) {
+        res = true;
+        return res;
+    }
+
+    if (g_pattern[pattern_idx] == '*') {
+        if (match(pattern_idx + 1, input_idx) || (input_idx < g_input.size() && match(pattern_idx, input_idx + 1))) {
+            res = true;
+            return res;
+        }
+    }
+
+    res = false;
+    return res;
+}
+
+void get_input()
+{
+    cin >> cnt;
+
+    for (int i = 0; i < cnt; i++) {
+        problem p;
+        int cnt_inputs;
+        cin >> p.pattern;
+        cin >> cnt_inputs;
+
+        for (int j = 0; j < cnt_inputs; j++) {
+            string input;
+            cin >> input;
+            p.words.push_back(input);
+        }
+        problems.push_back(p);
+    }
 }
 
 int main()
 {
-  get_input();
+    get_input();
 
-  for (auto problem : g_problems) {
-    g_pattern = problem.pattern;
+    for (auto& problem : problems) {
+        g_pattern = problem.pattern;
 
-    for (auto& word : problem.words) {
-      memset(cache, -1, sizeof(cache));
-      g_word = word;
+        vector<string> result;
+        for (auto& item : problem.words) {
+            memset(cache, -1, sizeof(cache));
+            g_input = item;
+            if (match(0, 0))
+                result.push_back(item);
+        }
+        sort(result.begin(), result.end());
+        for (auto& res : result)
+            cout << res << endl;
 
-      if (match(0, 0))
-        cout << g_word << endl;
     }
-  }
-  return 0;
+
+    return 0;
 }
