2edaeafb89629442369f9d9d8a139dd15b9553ef
Hyuk Myeong <hyuk.myeong@lge.com>
2021-03-12 18:42:03 +0900

    Update main.cpp

--- BOJ/1260/main.cpp
+++ BOJ/1260/main.cpp
@@ -2,55 +2,81 @@
 #include <vector>
 #include <queue>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
 class Graph {
 public:
-  Graph(int V) : vertice(V)
+  Graph(int N)
   {
-    adj.resize(V);
+    adj.resize(N+1);
+    visited.resize(N+1, false);
   }
 
-    void add_edge(int from, int to)
-    {
-      adj[from].push_back(to);
-    }
+  void add_edge(int from, int to)
+  {
+    adj[from].push_back(to);
+  }
 
-    void print_graph()
-    { for(int i=0; i<adj.size(); ++i) {
-        for(int j=0; j<adj[i].size(); ++j) {
+  void sort_graph()
+  {
+    for(int i=0; i<adj.size(); ++i)
+      sort(adj[i].begin(), adj[i].end(), less<int>());
+  }
+
+  void print_graph()
+  {
+    for(int i=1; i<adj.size(); ++i) {
+      for(int j=0; j<adj[i].size(); ++j) {
           cout << "from:" << i << ", to:" << adj[i][j] << endl;
+      }
+    }
+  }
+
+  void dfs_impl(int start)
+  {
+    cout << start << " ";
+    visited[start] = true;
+
+    for(int i=0; i<adj[start].size(); ++i) {
+      if(visited[adj[start][i]] == false)
+        dfs_impl(adj[start][i]);
+    }
+  }
+
+  void dfs(int start)
+  {
+    fill(visited.begin(), visited.end(), false);
+    dfs_impl(start);
+  }
+
+  void bfs(int start)
+  {
+    fill(visited.begin(), visited.end(), false);
+    std::queue<int> Q;
+    Q.push(start);
+
+    visited[0] = visited[start] = true;
+
+    while(Q.empty() == false) {
+      int cur = Q.front();
+      Q.pop();
+
+      cout << cur << " ";
+
+      for(int i=0; i<adj[cur].size(); ++i) {
+        if(visited[adj[cur][i]] == false) {
+          visited[adj[cur][i]] = true;
+          Q.push(adj[cur][i]);
         }
       }
     }
-
-    void bfs(int start) {
-      vector<bool> visited(adj.size(), false);
-      queue<int> Q;
-      int cur = start;
-
-      Q.push(start);
-
-      while(!Q.empty()) {
-        cur = Q.front();
-        visited[cur] = true;
-        Q.pop();
-
-        cout << cur << " visited" << endl;
-
-        for(int i=0; i<adj[cur].size(); ++i) {
-          cout << "i= " << i << "and adj[cur][i]=" << adj[cur][i] << endl;
-          if(visited[adj[cur][i]] == false)
-            Q.push(adj[cur][i]);
-        }
-      }
-
-    }
+  }
 
 private:
-  int vertice;
   vector<vector<int>> adj;
+  vector<bool> visited;
 };
 
 int main(int argc, char *argv[])
@@ -62,16 +88,20 @@ int main(int argc, char *argv[])
   int V, E, start;
   cin >> V >> E >> start;
 
-  Graph G(V+1);
+  Graph graph(V);
 
   int from, to;
   for(int i=0; i<E; ++i) {
     cin >> from >> to;
-    G.add_edge(from, to);
+    graph.add_edge(from, to);
+    graph.add_edge(to, from);
   }
 
-  G.print_graph();
-  G.bfs(start);
+  graph.sort_graph();
+  graph.dfs(start);
+  cout << endl;
+  graph.bfs(start);
+  cout << endl;
 
   return 0;
 }
